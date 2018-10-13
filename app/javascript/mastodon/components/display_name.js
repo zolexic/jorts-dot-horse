87543475a54493a9fabb7bb9d1b3f311cb7ef871
Zolexi <caerulic@gmail.com>
2018-10-13 00:52:57 -0500

    horsiefied display name
    work please work

--- app/javascript/mastodon/components/display_name.js
+++ app/javascript/mastodon/components/display_name.js
@@ -16,10 +16,15 @@ export default class DisplayName extends React.PureComponent {
   render () {
     const { account, withAcct } = this.props;
     const displayNameHtml = { __html: account.get('display_name_html') };
+    const acctName = this.props.account.get('acct');
 
+    function horsieCheck() {
+      const isJorts = !/\@/g.test(acctName);
+      return isJorts ? <img draggable='false' className='emojione' alt='Horsiefied' title='Horsiefied' src='https://media.jorts.horse/horse-media/custom_emojis/images/000/006/257/original/horsiefied.png' /> : '';
+    }
     return (
       <span className='display-name'>
-        <bdi><strong className='display-name__html' dangerouslySetInnerHTML={displayNameHtml} /></bdi> {withAcct && <span className='display-name__account'>@{account.get('acct')}</span>}
+        <bdi><strong className='display-name__html' dangerouslySetInnerHTML={displayNameHtml} /></bdi> {horsieCheck()} <span className='display-name__account'>@{acctName}</span>
       </span>
     );
   }
